e51c499e2cb9abbb8b9df5899ffea70c4a7f238b
Dihsar <ackrashid@gmail.com>
2023-06-01 18:04:36 -0400

    added help command

--- bot.js
+++ bot.js
@@ -4,4 +4,6 @@ const bot = new TeleBot(process.env.TOKEN);
 
 bot.on('/everyone', (msg) => msg.reply.text("@Dihsarr @benoji @paytoncollins @involutex @neeguss @Cranbaeri @puffpuff26 @Jayvid12 @mobu2 @p4rs33 @DimSum9000 @Yahootoyou @omegadeecee"));
 
+bot.on('/help', msg => msg.reply.text("/everyone - @'s every person in the chat"))
+
 bot.start();
