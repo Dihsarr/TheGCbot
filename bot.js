5c5b2ad0160d7743727d776605261d871e538a29
Dihsar <ackrashid@gmail.com>
2023-06-04 16:32:35 -0400

    Added catfact command

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env.TOKEN);
 
-bot.on('/everyone', async msg => {
+bot.on('/everyone', msg => {
     msg.reply.text("@Dihsarr @benoji @paytoncollins @involutex @neeguss @Cranbaeri @puffpuff26 @Jayvid12 @mobu2 @p4rs33 @DimSum9000 @Yahootoyou @omegadeecee")
 });
 
@@ -14,6 +14,18 @@ bot.on('/everyone', async msg => {
         }
         await bot.pinChatMessage(msg.chat.id, msg.reply_to_message.message_id)
     } catch(error){
+        console.log(error);
+    }
+ })
+
+ bot.on('/catfact', async msg => {
+    try{
+        const response = await fetch("https://meowfacts.herokuapp.com/")
+        const json = await response.json()
+        msg.reply.text(json.data[0])
+
+    }catch(error){
+        msg.reply.text('An Error occurred :(')
         console.log(error);
     }
  })
