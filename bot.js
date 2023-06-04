638aff69a320af3c099785c6f4cc8f7ba13fe910
Dihsar <ackrashid@gmail.com>
2023-06-04 15:35:26 -0400

    Removed help command and added pin command

--- bot.js
+++ bot.js
@@ -2,8 +2,20 @@ require('dotenv').config()
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env.TOKEN);
 
-bot.on('/everyone', (msg) => msg.reply.text("@Dihsarr @benoji @paytoncollins @involutex @neeguss @Cranbaeri @puffpuff26 @Jayvid12 @mobu2 @p4rs33 @DimSum9000 @Yahootoyou @omegadeecee"));
+bot.on('/everyone', async msg => {
+    msg.reply.text("@Dihsarr @benoji @paytoncollins @involutex @neeguss @Cranbaeri @puffpuff26 @Jayvid12 @mobu2 @p4rs33 @DimSum9000 @Yahootoyou @omegadeecee")
+});
 
-bot.on('/help', msg => msg.reply.text("/everyone - @'s every person in the chat"))
+ bot.on('/pin', async msg => {
+    try{
+        if(!msg.reply_to_message){
+            msg.reply.text("Use: Reply to the message you want to pin with /pin")
+            return
+        }
+        await bot.pinChatMessage(msg.chat.id, msg.reply_to_message.message_id)
+    } catch(error){
+        console.log(error);
+    }
+ })
 
 bot.start();
