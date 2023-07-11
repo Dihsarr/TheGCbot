9c6ba677d18923c7933558adcb165ad52105150c
Dihsar <ackrashid@gmail.com>
2023-07-11 17:26:47 -0400

    compleated bot controller file

--- bot.js
+++ bot.js
@@ -5,13 +5,15 @@ const User = require('./models/userModel')
 const Event = require('./models/eventModel')
 const {
     getCatFact,
+    pinMessage,
+    getInsult,
+    getAddress,
+    storeID,
+    addEvent,
+    getEvents,
+    editEvent,
+    deleteEvent,
 } = require("./botController")
-const {
-    userToID,
-    removeAtSymbol,
-    sendEventWithDelay,
-    findAndSendEvent,
-} = require("./utilityFunctions")
 const {
     sendEveryone,
     setAddress
@@ -23,219 +25,35 @@ const bot = new TeleBot(process.env.TOKEN);
 //@'s every person in the telegram chat
 bot.on('/everyone', async msg => {sendEveryone(msg,bot)})  
 
-
 //pins a message to the currect chat
-bot.on('/pin', async msg =>{
-    //if message is not sent as reply give error
-    try{
-        if(!msg.reply_to_message){
-            msg.reply.text("Use: Reply to the message you want to pin with /pin")
-            return
-        }
-        await bot.pinChatMessage(msg.chat.id, msg.reply_to_message.message_id)
-    } catch(error){
-        console.log(error);
-    }
- } )
+bot.on('/pin', async msg => pinMessage(msg, bot) )
 
  //uses Cat fact api to grab a random cat fact
- bot.on('/catfact', getCatFact)
+bot.on('/catfact', getCatFact)
 
  //uses insult api to grab a random insult
- bot.on('/insult', async msg => {
-    try{
-        const response = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json")
-        const json = await response.json()
-        //if command is sent as reply the insult is sent as a reply to the original message
-        if(msg.reply_to_message){
-            await bot.sendMessage(msg.chat.id,json.insult, {replyToMessage: msg.reply_to_message.message_id})
-            return 
-        }
-        msg.reply.text(json.insult)
-
-    }catch(error){
-        msg.reply.text('An Error occurred :(')
-        console.log(error);
-    }
- })
+bot.on('/insult', async msg => getInsult(msg,bot) )
 
  //use to set another users address
 bot.on(/^\/setaddress (\S+)\s(.+)$/i, async (msg, props) => setAddress(msg,props,bot))
 
-  bot.on(/^\/address (.+)$/, async (msg, props) =>{
+//grabs a users address
+bot.on(/^\/address (.+)$/, async (msg, props) => getAddress(msg,props,bot))
 
-    const username = removeAtSymbol(props.match[1])
-    const targetUserId = await userToID(username)
+//stores a users id and their username
+bot.on('/storeid', async msg => storeID(msg) )
 
-    const response = await Address.findOne({setUser: msg.from.id, targetUser: targetUserId })
-    if(!response){
-        bot.sendMessage(msg.from.id,"Address not found, use /setaddress")
-        return
-    }
-    bot.sendMessage(msg.from.id,`Address: ${response.address}`)
-    return
-  })
+//adds an event
+ bot.on(/^\/addevent \[(.+)] \[(.+)] \[(.+)] (.+)$/i, async (msg, props) => addEvent(msg,props))
 
-  bot.on('/storeid', async msg => {
-    const fromUser = msg.from.id
-    const user = await User.findOne({id: fromUser})
-    if(user){
-        console.log(user);
-        msg.reply.text('User already stored')
-        return
-    }
-    await User.create({
-       id: fromUser,
-       username: msg.from.username
-    })
-    .then(() => {
-       console.log('User set')
-       msg.reply.text('User sucessfully set')
-   })
-    .catch((error) => console.log(error._message))
- })
+ // can grab created events
+bot.on(/^\/events (.+)$/, async (msg,props) => getEvents(msg,props) )
 
- bot.on(/^\/addevent \[(.+)] \[(.+)] \[(.+)] (.+)$/i, async (msg, props) => {
-    const eventName = props.match[1]
-    const eventLocation = props.match[2]
-    const eventDate =  new Date(props.match[3] + ' GMT')
-    const eventDescription = props.match[4]
+//used to edit events
+bot.on(/^\/editevent \[(.+)] \[(.+)] \[(.+)]/, async (msg,props) => editEvent(msg,props))
 
-    
-    await Event.create({
-        eventName,
-        eventLocation,
-        eventDate,
-        eventDescription
-    })
-    .then(() => {
-        msg.reply.text("Event Sucessfully added!")
-        findAndSendEvent(msg,eventName)
-        
-    })
-    .catch(error => {
-        if(error.errors.eventDate.valueType === 'Date'){
-            msg.reply.text('Invalid Date format: [mm/dd/yyyy hh:mm am/pm]')
-        }
-    })
-})
-
-bot.on(/^\/events (.+)$/, async (msg,props) => {
-    const prop = props.match[1] 
-    var filterDays = 0
-
-    //filter date will be used to set date to filter by
-    switch (prop) {
-        case 'all':
-            filterDays = 9999999
-            break;
-        case 'week':
-            filterDays = 7
-            break;
-        case 'month':
-            filterDays = 30
-            break;
-        default:
-            msg.reply.text('Invalid timescale use: all/week/month')
-            return
-    }
-            //generates a date filterDays days away
-            const endFilterDate = new Date()
-            const startFilterDate = new Date()
-            startFilterDate.setDate(startFilterDate.getDate() - 1)
-            endFilterDate.setDate(endFilterDate.getDate() + filterDays)
-
-
-            var response = await Event.find({})
-            const filteredEvents = []
-
-            //adds event to filteredEvents if it falls before the filter Date
-            response.forEach((event) => {
-                if((endFilterDate - event.eventDate) > 0 && (startFilterDate - event.eventDate) < 0 ){
-                    filteredEvents.push(event)
-                }
-            })
-
-            
-            //cases for if no events are found before the filter date
-            if(filteredEvents.length === 0){
-               if(prop === 'all'){
-                msg.reply.text('No events found :( add one with /addevent')
-                return
-               }https://cloud.mongodb.com/v2/6438357ca77e5427e4f1c3e2#/security/network
-               msg.reply.text(`No events found for the ${prop}.`)
-               return
-            }
-
-            filteredEvents.sort((a,b) => a.eventDate - b.eventDate)
-
-        //used of instead of foreach so await could be used
-        for (const event of filteredEvents) {
-            await sendEventWithDelay(msg,event);
-        }   
-            
-       }
-      
-)
-
-
-bot.on(/^\/editevent \[(.+)] \[(.+)] \[(.+)]/, async (msg,props) => {
-
-    
-    //store props
-    const eventName = props.match[1]
-    const fieldToEdit = props.match[2].toLowerCase()
-    var editedContent = props.match[3]
-    
- 
-    console.log(editedContent);
-    var schemaToChange = ''
-
-    //changes feildToEdit to match Event Schema 
-    switch(fieldToEdit){
-        case 'name':
-            schemaToChange = {eventName: editedContent}
-            break
-        case 'location':
-            schemaToChange = {eventLocation: editedContent}
-            break
-        case 'date':
-            editedContent = new Date(editedContent + ' GMT')
-            schemaToChange = {eventDate: editedContent}
-            break
-        case 'description':
-            schemaToChange = {eventDescription: editedContent}
-            break
-        default :
-            msg.reply.text('Must enter valid field to change name/location/date/description')
-            return
-    }
-
-    //checks if document exists and updates it if it does
-    const response =  await Event.findOneAndUpdate({eventName}, schemaToChange, {new: true} )
-    if (!response){
-        msg.reply.text('Event not found')
-        return
-    }
-    msg.reply.text('Event updated!')
-    sendEventWithDelay(msg,response)
-
-})
-
-bot.on(/^\/delevent \[(.+)]/, async (msg,props) => {
-    //assine varibles from props
-    const eventToDelete = props.match[1]
-
-    //find and delete event
-    const response = await Event.findOneAndDelete({eventName: eventToDelete})
-    if(!response){
-        msg.reply.text('Event not found')
-    }
-    msg.reply.text('--- Removed Event ---')
-    sendEventWithDelay(msg,response)
-
-    
-})
+//deletes a created event
+bot.on(/^\/delevent \[(.+)]/, async (msg,props) => deleteEvent(msg, props) )
 
  // a dev command
  bot.on('/devTest', msg => console.log(msg))
--- botController.js
+++ botController.js
@@ -2,6 +2,13 @@ const User = require('./models/userModel')
 const Event = require('./models/eventModel')
 const Address = require('./models/addressModel')
 
+const {
+    userToID,
+    removeAtSymbol,
+    sendEventWithDelay,
+    findAndSendEvent,
+} = require("./utilityFunctions")
+
 const sendEveryone = async (msg,bot) => {
     if(msg.from.id === 1835387722 ){
       msg.reply.text('Nah')
@@ -15,20 +22,48 @@ const sendEveryone = async (msg,bot) => {
     }
 
 
+const pinMessage = async (msg, bot) => {
+    //if message is not sent as reply give error
+    try{
+        if(!msg.reply_to_message){
+            msg.reply.text("Use: Reply to the message you want to pin with /pin")
+            return
+        }
+        await bot.pinChatMessage(msg.chat.id, msg.reply_to_message.message_id)
+    } catch(error){
+        console.log(error);
+    }
+}
 
+const getCatFact = async msg => {
+    try{
+        const response = await fetch("https://meowfacts.herokuapp.com/")
+        const json = await response.json()
+        msg.reply.text(json.data[0])
 
+    }catch(error){
+        msg.reply.text('An Error occurred :(')
+        console.log(error);
+    }
+ }
 
+const getInsult = async (msg, bot) => {
+    try{
+        const response = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json")
+        const json = await response.json()
+        //if command is sent as reply the insult is sent as a reply to the original message
+        if(msg.reply_to_message){
+            await bot.sendMessage(msg.chat.id,json.insult, {replyToMessage: msg.reply_to_message.message_id})
+            return 
+        }
+        msg.reply.text(json.insult)
 
+    }catch(error){
+        msg.reply.text('An Error occurred :(')
+        console.log(error);
+    }
+ }
 
-
-
-
-
-
-
-
-
-    
 //sets a users address
 const setAddress = async (msg,props,bot) => {
 
@@ -75,8 +110,183 @@ const setAddress = async (msg,props,bot) => {
      .catch((error) => console.log(error._message))
   }
 
+const getAddress = async (msg,props,bot) => {
+    
+    const username = removeAtSymbol(props.match[1])
+    const targetUserId = await userToID(username)
+
+    const response = await Address.findOne({setUser: msg.from.id, targetUser: targetUserId })
+    if(!response){
+        bot.sendMessage(msg.from.id,"Address not found, use /setaddress")
+        return
+    }
+    bot.sendMessage(msg.from.id,`Address: ${response.address}`)
+    return
+}
+
+const storeID = async msg => {
+    const fromUser = msg.from.id
+    const user = await User.findOne({id: fromUser})
+    if(user){
+        console.log(user);
+        msg.reply.text('User already stored')
+        return
+    }
+    await User.create({
+       id: fromUser,
+       username: msg.from.username
+    })
+    .then(() => {
+       console.log('User set')
+       msg.reply.text('User sucessfully set')
+   })
+    .catch((error) => console.log(error._message))
+}
+
+const addEvent = async (msg, props) => {
+    const eventName = props.match[1]
+    const eventLocation = props.match[2]
+    const eventDate =  new Date(props.match[3] + ' GMT')
+    const eventDescription = props.match[4]
+
+    
+    await Event.create({
+        eventName,
+        eventLocation,
+        eventDate,
+        eventDescription
+    })
+    .then(() => {
+        msg.reply.text("Event Sucessfully added!")
+        findAndSendEvent(msg,eventName)
+        
+    })
+    .catch(error => {
+        if(error.errors.eventDate.valueType === 'Date'){
+            msg.reply.text('Invalid Date format: [mm/dd/yyyy hh:mm am/pm]')
+        }
+    })
+}
+
+const getEvents = async (msg, props) => {
+    const prop = props.match[1] 
+    var filterDays = 0
+
+    //filter date will be used to set date to filter by
+    switch (prop) {
+        case 'all':
+            filterDays = 9999999
+            break;
+        case 'week':
+            filterDays = 7
+            break;
+        case 'month':
+            filterDays = 30
+            break;
+        default:
+            msg.reply.text('Invalid timescale use: all/week/month')
+            return
+    }
+            //generates a date filterDays days away
+            const endFilterDate = new Date()
+            const startFilterDate = new Date()
+            startFilterDate.setDate(startFilterDate.getDate() - 1)
+            endFilterDate.setDate(endFilterDate.getDate() + filterDays)
+
+
+            var response = await Event.find({})
+            const filteredEvents = []
+
+            //adds event to filteredEvents if it falls before the filter Date
+            response.forEach((event) => {
+                if((endFilterDate - event.eventDate) > 0 && (startFilterDate - event.eventDate) < 0 ){
+                    filteredEvents.push(event)
+                }
+            })
+
+            
+            //cases for if no events are found before the filter date
+            if(filteredEvents.length === 0){
+               if(prop === 'all'){
+                msg.reply.text('No events found :( add one with /addevent')
+                return
+               }https://cloud.mongodb.com/v2/6438357ca77e5427e4f1c3e2#/security/network
+               msg.reply.text(`No events found for the ${prop}.`)
+               return
+            }
+
+            filteredEvents.sort((a,b) => a.eventDate - b.eventDate)
+
+        //used of instead of foreach so await could be used
+        for (const event of filteredEvents) {
+            await sendEventWithDelay(msg,event);
+        }
+}
+
+const editEvent = async (msg, props) => {
+        
+    //store props
+    const eventName = props.match[1]
+    const fieldToEdit = props.match[2].toLowerCase()
+    var editedContent = props.match[3]
+    
+ 
+    console.log(editedContent);
+    var schemaToChange = ''
+
+    //changes feildToEdit to match Event Schema 
+    switch(fieldToEdit){
+        case 'name':
+            schemaToChange = {eventName: editedContent}
+            break
+        case 'location':
+            schemaToChange = {eventLocation: editedContent}
+            break
+        case 'date':
+            editedContent = new Date(editedContent + ' GMT')
+            schemaToChange = {eventDate: editedContent}
+            break
+        case 'description':
+            schemaToChange = {eventDescription: editedContent}
+            break
+        default :
+            msg.reply.text('Must enter valid field to change name/location/date/description')
+            return
+    }
+
+    //checks if document exists and updates it if it does
+    const response =  await Event.findOneAndUpdate({eventName}, schemaToChange, {new: true} )
+    if (!response){
+        msg.reply.text('Event not found')
+        return
+    }
+    msg.reply.text('Event updated!')
+    sendEventWithDelay(msg,response)
+}
+
+const deleteEvent = async (msg, props) => {
+    //assine varibles from props
+    const eventToDelete = props.match[1]
+
+    //find and delete event
+    const response = await Event.findOneAndDelete({eventName: eventToDelete})
+    if(!response){
+        msg.reply.text('Event not found')
+    }
+    msg.reply.text('--- Removed Event ---')
+    sendEventWithDelay(msg,response)
+}
 
 module.exports = {
     sendEveryone,
-    setAddress
+    pinMessage,
+    getCatFact,
+    getInsult,
+    setAddress,
+    getAddress,
+    storeID,
+    addEvent,
+    getEvents,
+    editEvent,
+    deleteEvent
 }
